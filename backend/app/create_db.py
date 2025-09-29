01a04a0b2522904a8a5bccb04ce70762a9a28240
Brandon Arrendondo <brandon.arrendondo@bissell.com>
2025-09-29 07:08:27 -0400

    merged

--- backend/app/create_db.py
+++ backend/app/create_db.py
@@ -9,7 +9,6 @@ from models.base import (
 from sqlalchemy_utils import database_exists, create_database
 from database import engine
 
-
 if not database_exists(engine.url):
     create_database(engine.url)
     print("Created database.")
